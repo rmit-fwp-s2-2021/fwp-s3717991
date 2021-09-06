4fe8c98658e9b2c2c29086bbc6011c65476011e4
minimuscle <joshthiele@live.com.au>
2021-09-06 17:35:47 +1000

    editing and deleting posts

--- vibecheck/src/pages/Posts.js
+++ vibecheck/src/pages/Posts.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Container, Row, Card, Modal, Button, Form } from "react-bootstrap";
-import EditUser from "../components/EditUser";
 
 export default function Posts(props) {
   const [edit, setEdit] = useState(false);
   const [show, setShow] = useState(false);
   const [thoughts, setThoughts] = useState("");
+  const [oldThoughts, setOldThoughts] = useState("");
+  const [index, setIndex] = useState(0);
 
   //TODO: ask database if data is there, so a try-catch loop will not be needed. 
   let name;
@@ -16,33 +17,62 @@ export default function Posts(props) {
     if (posts === null) {
       console.log("NULL");
       posts = [{
+        id: 0,
         name: "No Posts",
         post: "There are no posts. Make some more!",
-        time: 0
       }];
     }
-    posts.reverse();
+    //posts.reverse();
     console.log(posts);
     props.setloggedInStatus;
   } catch (e) {
     console.log(e);
   }
 
-  function shown() {
-    setEdit(!edit);
-  }
-
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
-  function handleSubmit() {
-    //event.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
     console.log(name, thoughts);
     let time = new Date();
-    posts.push({ name: name, post: thoughts, time: time });
+    //gets the latest ID
+    let id = posts[posts.length - 1].id;
+    posts.push({ id: id + 1, name: name, post: thoughts, time: time });
     let json = JSON.stringify(posts);
     localStorage.setItem("posts", json);
     console.log(posts);
+  }
+
+  function editPost(id, post) {
+    setOldThoughts(post);
+    setIndex(id);
+    setEdit(true);
+  }
+
+  function handleEdit() {
+    const objIndex = posts.map(obj =>
+      obj.id === index
+        ? { ...obj, post: oldThoughts }
+        : obj
+    );
+    let json = JSON.stringify(objIndex);
+    localStorage.setItem("posts", json);
+    setEdit(false);
+  }
+
+  function handleShow(id) {
+    setIndex(id);
+    setShow(true);
+  }
+
+  function handleDelete() {
+    let filtered = posts.filter(
+      function (el) {
+        return el.id != index;
+      });
+    let json = JSON.stringify(filtered);
+    localStorage.setItem("posts", json);
+    setShow(false);
   }
 
   return (
@@ -51,25 +81,35 @@ export default function Posts(props) {
         <Container>
 
           <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton><Modal.Title>Delete User</Modal.Title></Modal.Header>
-            <Modal.Body><p>Are you sure you want to delete this user?</p></Modal.Body>
+            <Modal.Header closeButton><Modal.Title>Delete Post</Modal.Title></Modal.Header>
+            <Modal.Body><p>Are you sure you want to delete this post?</p></Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary">Yes, Delete</Button>
+              <Button variant="secondary" onClick={handleDelete}>Yes, Delete</Button>
               <Button variant="primary" onClick={handleClose}>No</Button>
             </Modal.Footer>
           </Modal>
 
-          {edit ? <EditUser shown={edit} handleClose={shown} /> : ""}
+          <Modal show={edit} onHide={() => setEdit(false)}>
+            <Modal.Header closeButton><Modal.Title>Edit Post</Modal.Title></Modal.Header>
+            <Modal.Body>
+              <Form onSubmit={handleEdit}>
+                <Form.Group size="lg">
+                  <Form.Control autoFocus as="textarea" rows={5} value={oldThoughts} onChange={(e) => setOldThoughts(e.target.value)} required /><br />
+                </Form.Group>
+                <Button type="submit">
+                  Edit Post
+                </Button>
+              </Form>
+            </Modal.Body>
+          </Modal>
           <Row>
             <div className="text-area">
               <Card>
-                <Card.Header>{name}
-                  <span><a href="#">EDIT</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onClick={handleShow}>DELETE</a></span>
-                </Card.Header>
+                <Card.Header>{name}</Card.Header>
                 <Card.Body>
                   <Form onSubmit={handleSubmit}>
-                    <Form.Group size="lg" controlId="email">
-                      <Form.Control autoFocus as="textarea" rows={5} placeholder="Share your thoughts..." value={thoughts} onChange={(e) => setThoughts(e.target.value)} required/><br />
+                    <Form.Group size="lg" controlId="post">
+                      <Form.Control autoFocus as="textarea" rows={5} placeholder="Share your thoughts..." value={thoughts} onChange={(e) => setThoughts(e.target.value)} required /><br />
                     </Form.Group>
                     <Button type="submit">
                       Post
@@ -80,19 +120,21 @@ export default function Posts(props) {
 
 
               <div className="posts">
-                {posts.map((data, key) => {
-                  return (
-                    <div key={key}>
-                      <Card>
-                        <Card.Body>
-                          <Card.Title>{data.name}</Card.Title>
-                          <Card.Text>{data.post}</Card.Text>
-                        </Card.Body>
-                        <Card.Footer className="text-muted">{data.time}</Card.Footer>
-                      </Card>
-                    </div>
-                  );
-                })}
+                {
+                  posts.map((data, key) => {
+                    return (
+                      <div key={key}>
+                        <Card>
+                          <Card.Body>
+                            <Card.Title>{data.name}: {data.id}
+                              <span><a href="#" onClick={() => editPost(data.id, data.post)}>EDIT</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" onClick={() => handleShow(data.id)}>DELETE</a></span></Card.Title>
+                            <Card.Text>{data.post}</Card.Text>
+                          </Card.Body>
+                          <Card.Footer className="text-muted">{data.time}</Card.Footer>
+                        </Card>
+                      </div>
+                    );
+                  })}
               </div>
             </div>
           </Row>
