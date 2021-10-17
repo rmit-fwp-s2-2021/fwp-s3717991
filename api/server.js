2096c5f916f77b755e6dc2692528c3bbfed93cef
minimuscle <joshthiele@live.com.au>
2021-10-18 10:19:41 +1100

    added cookies to system

--- api/server.js
+++ api/server.js
@@ -1,14 +1,13 @@
 const db = require('./database/app.js')
 const express = require('express')
-const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 //Syncs Database
 db.sync()
 
 const app = express()
 app.use(express.json())
-app.use(cors())
-
+app.use(cookieParser())
 // Adds Routes //
 
 //Default route
